5752ee6341c5618645df3bf3fabf32744a861ad1
Marcin Płużyński <marcinp2012@gmail.com>
2018-04-26 11:34:20 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className='container'>
+                App Component
+            </div>
+        );
+    }
+}
+
+export default App;
