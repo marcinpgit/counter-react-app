d08d54cc9b08594eb9be5a74b4b07a498cd4c53b
Marcin Płużyński <marcinp2012@gmail.com>
2018-04-26 15:31:11 +0200

    Incuded all components in App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
+import Counter from './container/Counter';
+import AddCounter from './container/AddCounter';
+import RemoveCounter from './container/RemoveCounter';
+
 class App extends React.Component {
     render() {
         return (
             <div className='container'>
-                App Component
+                <Counter/>
+                <div className='columns'>
+                    <div className='column is-11'>
+                        <AddCounter/>
+                    </div>
+                    <div className='column auto'>
+                        <RemoveCounter/>
+                    </div>
+                </div>
             </div>
         );
     }
